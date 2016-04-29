9f3c657b4b5c1c283420d667b2cb86699249a449
SSO <samy.soualem@gmail.com>
2016-04-29 22:18:46 +0200

    Changed title

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 require(shiny)
 
 shinyUI(pageWithSidebar(
-  headerPanel("US State Facts and Figures from the \"states\" dataset in R"),
+  headerPanel("Visualising US State Facts and Figures with googleVis"),
   sidebarPanel(
     selectInput("stateVar", "Choose a statistic to plot:", 
                 choices = stateVarInputNm),
@@ -10,7 +10,9 @@ shinyUI(pageWithSidebar(
     
     textOutput("varHelpTxt"),
     
-    helpText("Hover the mouse over a state to see the precise figure")
+    helpText("Hover the mouse over a state to see the precise figure"),
+    
+    helpText("Source : \"state\" dataset in R")
 
   ),
   mainPanel(
